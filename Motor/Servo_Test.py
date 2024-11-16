6b96dbcb58ef06d15a1c1eb7c1372e096ff57029
dawntab <albugi0530@gmail.com>
2024-11-16 12:57:39 +0900

    Add  Servo.py for servo motor test

---
+++ Motor/Servo_Test.py
@@ -0,0 +1,34 @@
+import Jetson.GPIO as GPIO
+import time
+
+# 서보모터 연결 핀 설정
+SERVO_PIN = 18  
+
+# GPIO 설정
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(SERVO_PIN, GPIO.OUT)
+
+# PWM 객체 생성 
+pwm = GPIO.PWM(SERVO_PIN, 50)
+
+# PWM 시작 
+pwm.start(90)
+time.sleep(2)  # 서보모터 초기화 시간 대기
+
+# 각도 -> Duty Cycle 변환 함수
+def angle_to_duty_cycle(angle):
+    # 서보모터의 동작 범위 (0.5ms ~ 2.5ms, 50Hz 기준 Duty Cycle = 2.5% ~ 12.5%)
+    return 2.5 + (angle / 180.0) * 10.0
+
+
+while True:
+    angle = int(input("angle(0 ~ 180): "))
+
+    if not(0 <= angle <= 180):
+        break
+    
+    pwm.ChangeDutyCycle(angle_to_duty_cycle(angle))
+    
+
+pwm.stop()
+GPIO.cleanup()
